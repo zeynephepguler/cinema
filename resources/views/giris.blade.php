8e090440451d0f73af5c3e7982f694488e61c106
MehtapAyall <mehtapayal60@gmail.com>
2022-03-04 21:02:57 +0300

    giriş panali

---
+++ resources/views/giris.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/baglanti.css') }}">
+    <title></title>
+  </head>
+  <body class="üst">
+    <div class="alan"> <center>
+      <h1>X-SİNEMA SİSTEMİ GİRİŞ PANALİ</h1>
+      <table>
+        <tr> <td> Kullanıcı Adı: </td><th> <input type="text" name="" value=""> </th></tr>
+        <tr> <td >Şifre: </td><th> <input type="text" name="" value=""> </th></tr>
+      </table><br>
+      <button type="button" name="btn_giris" class="btn_giris"> GİRİŞ YAP</button>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', function()
 {
-  return view('anasayfa');
+  return view('giris');
 })->name('cıkıs');
 
 Route::get('ogrencigiris', function()
