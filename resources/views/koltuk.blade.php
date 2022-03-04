a018ef8123a04229291427637b6a37939fb12953
MehtapAyall <mehtapayal60@gmail.com>
2022-03-04 21:11:30 +0300

    arka

--- resources/views/koltuk.blade.php
+++ resources/views/koltuk.blade.php
@@ -1,199 +1,208 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <link rel="stylesheet" href="{{ asset('css/a.css') }}" type="text/css">
+      <link rel="stylesheet" href="{{ asset('css/baglanti.css') }}" type="text/css">
+    <title></title>
+  </head>
+  <body class="üst">
+    <div>
+        <center>
+    <dialog id="koltuksec" style="width:60%; background-color:#abcecd; border:1px dotted black;"
+    >
+    <table >
+
+    <tr>
+      <th></th><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th><th>8</th><th>9</th><th>10</th>
+    </tr>
+
+    <tr >
+    <td><h1> A</h1></td><td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" select" >
+    </td><td><img class="img" id="a2"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a3"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+
+    </tr>
+
+    <tr>
+    <td><h1> B</h1></td><td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" select" ></td>
+    <td><img class="img" id="a2"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a3"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+
+    <tr>
+    <td><h1> C</h1></td><td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" select" ></td>
+    <td><img class="img" id="a2"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a3"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+
+    </tr>
+    <tr>
+
+    <td><h1> D</h1></td><td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" select" ></td>
+    <td><img class="img" id="a2"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a3"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+    <td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
+
+    </tr>
+    <table><tr>
+    <td><button class="button">12:00</button></td>
+    <td><button class="button">13:00</button></td>
+    <td><button class="button">14:00</button></td>
+    <td><button class="button">15:00</button></td>
+    </tr></table>
+    </table>
 
 
+    <button id="hide">Pencereyi Kapat</button>
+    </dialog>
+    </center>
 
-<div>
-    <center>
-<dialog id="koltuksec" style="width:60%; background-color:#abcecd; border:1px dotted black;"  
->
-<table >
-    <link rel="stylesheet" href="{{ asset('css/a.css') }}" type="text/css">
-<tr>
-  <th></th><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th><th>8</th><th>9</th><th>10</th>
-</tr>
-
-<tr >
-<td><h1> A</h1></td><td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" select" >
-</td><td><img class="img" id="a2"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a3"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-
-</tr>
-
-<tr>
-<td><h1> B</h1></td><td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" select" ></td>
-<td><img class="img" id="a2"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a3"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-
-<tr>
-<td><h1> C</h1></td><td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" select" ></td>
-<td><img class="img" id="a2"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a3"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-
-</tr>
-<tr>
-
-<td><h1> D</h1></td><td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" select" ></td>
-<td><img class="img" id="a2"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a3"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-<td><img class="img" id="a1"src="{{asset('koltuk.png')}}" height="50px" width="50px" ></td>
-
-</tr>
-<table><tr>
-<td><button class="button">12:00</button></td>
-<td><button class="button">13:00</button></td>
-<td><button class="button">14:00</button></td>
-<td><button class="button">15:00</button></td>
-</tr></table>
-</table>
-
-
-<button id="hide">Pencereyi Kapat</button>
-</dialog>
-</center>
-
-    <center>
-<table>
-    <tr><td>
-        <table>
-            <tr><th>filmler</th><th>saatler</th></tr>
-    
-            <tr><td><img src="{{asset('ayla.jpg')}}" height="300px" width="200px"></td><td>
-           <div class="bilgi">
-                <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
-                <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
-                <p>Yönetmen Can Ulkay</p>
-                <p>Senarist Yiğit Güralp</p>
-                <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
-                <button id="show">İçeriği Göster</button>
-            </div>
-                
-
-
-
-</td>
-
-    
-        </tr>
-        <tr>
-            <td><img src="{{asset('bisi.jpg')}}" height="300px" width="200px"></td>
-            </td><td>  <div class="bilgi">
-                <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
-                <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
-                <p>Yönetmen Can Ulkay</p>
-                <p>Senarist Yiğit Güralp</p>
-                <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
-                <button id="show">İçeriği Göster</button>
-            </div></td>
-            </tr><tr><td><img src="{{asset('canakkale.jpg')}}" height="300px" width="200px"></td>
-            </td><td>  <div class="bilgi">
-                <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
-                <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
-                <p>Yönetmen Can Ulkay</p>
-                <p>Senarist Yiğit Güralp</p>
-                <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
-                <button id="show">İçeriği Göster</button>
-            </div></td>
-            </tr><tr><td><img src="{{asset('convik.jpg')}}" height="300px" width="200px"></td>
-            </td><td>  <div class="bilgi">
-                <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
-                <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
-                <p>Yönetmen Can Ulkay</p>
-                <p>Senarist Yiğit Güralp</p>
-                <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
-                <button id="show">İçeriği Göster</button>
-            </div></td>
-
-                </table></td><td><table>
+        <center>
+    <table>
+        <tr><td>
+            <table>
                 <tr><th>filmler</th><th>saatler</th></tr>
-    
-                <tr><td><img src="{{asset('dilberay.jpg')}}" height="300px" width="200px">
+
+                <tr><td><img src="{{asset('ayla.jpg')}}" height="300px" width="200px"></td><td>
+               <div class="bilgi">
+                    <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
+                    <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
+                    <p>Yönetmen Can Ulkay</p>
+                    <p>Senarist Yiğit Güralp</p>
+                    <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
+                    <button id="show">İçeriği Göster</button>
+                </div>
+
+
+
+
+    </td>
+
+
+            </tr>
+            <tr>
+                <td><img src="{{asset('bisi.jpg')}}" height="300px" width="200px"></td>
                 </td><td>  <div class="bilgi">
-                <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
-                <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
-                <p>Yönetmen Can Ulkay</p>
-                <p>Senarist Yiğit Güralp</p>
-                <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
-                <button id="show">İçeriği Göster</button>
-            </div></td>
-
-    
-                </tr>
-                <tr><td><img src="{{asset('gonul.jpg')}}" height="300px" width="200px"></td>
-                </td><td> <div class="bilgi">
-                <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
-                <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
-                <p>Yönetmen Can Ulkay</p>
-                <p>Senarist Yiğit Güralp</p>
-                <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
-                <button id="show">İçeriği Göster</button>
-            </div></td>
-                </tr><tr><td><img src="{{asset('hobbit.jpg')}}" height="300px" width="200px"></td>
+                    <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
+                    <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
+                    <p>Yönetmen Can Ulkay</p>
+                    <p>Senarist Yiğit Güralp</p>
+                    <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
+                    <button id="show">İçeriği Göster</button>
+                </div></td>
+                </tr><tr><td><img src="{{asset('canakkale.jpg')}}" height="300px" width="200px"></td>
                 </td><td>  <div class="bilgi">
-                <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
-                <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
-                <p>Yönetmen Can Ulkay</p>
-                <p>Senarist Yiğit Güralp</p>
-                <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
-                <button id="show">İçeriği Göster</button>
-            </div></td>
-                </tr><tr><td><img src="{{asset('joker.jpg')}}" height="300px" width="200px"></td>
+                    <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
+                    <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
+                    <p>Yönetmen Can Ulkay</p>
+                    <p>Senarist Yiğit Güralp</p>
+                    <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
+                    <button id="show">İçeriği Göster</button>
+                </div></td>
+                </tr><tr><td><img src="{{asset('convik.jpg')}}" height="300px" width="200px"></td>
                 </td><td>  <div class="bilgi">
-                <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
-                <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
-                <p>Yönetmen Can Ulkay</p>
-                <p>Senarist Yiğit Güralp</p>
-                <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
-                <button id="show">İçeriği Göster</button>
-            </div></td>
+                    <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
+                    <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
+                    <p>Yönetmen Can Ulkay</p>
+                    <p>Senarist Yiğit Güralp</p>
+                    <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
+                    <button id="show">İçeriği Göster</button>
+                </div></td>
 
-                    </td></tr>
-</table>
+                    </table></td><td><table>
+                    <tr><th>filmler</th><th>saatler</th></tr>
+
+                    <tr><td><img src="{{asset('dilberay.jpg')}}" height="300px" width="200px">
+                    </td><td>  <div class="bilgi">
+                    <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
+                    <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
+                    <p>Yönetmen Can Ulkay</p>
+                    <p>Senarist Yiğit Güralp</p>
+                    <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
+                    <button id="show">İçeriği Göster</button>
+                </div></td>
 
 
-<div >
-     
-    
-   
-    
+                    </tr>
+                    <tr><td><img src="{{asset('gonul.jpg')}}" height="300px" width="200px"></td>
+                    </td><td> <div class="bilgi">
+                    <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
+                    <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
+                    <p>Yönetmen Can Ulkay</p>
+                    <p>Senarist Yiğit Güralp</p>
+                    <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
+                    <button id="show">İçeriği Göster</button>
+                </div></td>
+                    </tr><tr><td><img src="{{asset('hobbit.jpg')}}" height="300px" width="200px"></td>
+                    </td><td>  <div class="bilgi">
+                    <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
+                    <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
+                    <p>Yönetmen Can Ulkay</p>
+                    <p>Senarist Yiğit Güralp</p>
+                    <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
+                    <button id="show">İçeriği Göster</button>
+                </div></td>
+                    </tr><tr><td><img src="{{asset('joker.jpg')}}" height="300px" width="200px"></td>
+                    </td><td>  <div class="bilgi">
+                    <h4>27 Ekim 2017 Sinemada / 2s 4dk / Dram, Savaş filmi</h4>
+                    <p>Yeniden vizyona giriş tarihi: 15 Haziran 2018</p>
+                    <p>Yönetmen Can Ulkay</p>
+                    <p>Senarist Yiğit Güralp</p>
+                    <p>Oyuncular: İsmail Hacıoğlu, Çetin Tekindor, Kim Seol</p>
+                    <button id="show">İçeriği Göster</button>
+                </div></td>
 
-</div>
-</center>
+                        </td></tr>
+    </table>
 
-</div>
-<script type="text/JavaScript">
-(function() {
-    var dialog = document.getElementById('koltuksec');
-    document.getElementById('show').onclick = function() {
-        dialog.show();
-    };
-    document.getElementById('hide').onclick = function() {
-        dialog.close();
-    };
-})();
-</script>
+
+    <div >
+
+
+
+
+
+    </div>
+    </center>
+
+    </div>
+    <script type="text/JavaScript">
+    (function() {
+        var dialog = document.getElementById('koltuksec');
+        document.getElementById('show').onclick = function() {
+            dialog.show();
+        };
+        document.getElementById('hide').onclick = function() {
+            dialog.close();
+        };
+    })();
+    </script>
+
+  </body>
+</html>
