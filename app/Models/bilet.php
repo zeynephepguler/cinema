cd6a4a316e5716ccdb780a831ed6ede031a34b35
zeyniep <75210764+zeyniep@users.noreply.github.com>
2022-03-15 22:10:44 +0300

    esrsgrgr

---
+++ app/Models/bilet.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class bilet extends Model
+{
+    use HasFactory;
+}
