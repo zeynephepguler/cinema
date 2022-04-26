681cae982c562c4e791c7e70b154800f836be2e0
zeyniep <75210764+zeyniep@users.noreply.github.com>
2022-04-26 23:03:58 +0300

    dsv

---
+++ app/Models/kullan.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Validator, Input, Redirect;
+use App\Models\admin;
+use App\Models\musteri;
+use App\Models\bilet;
+use App\Models\film;
+
+
+class kullan extends Controller
+{
+    function kontrol(Request $request){
+        //Validate requests
+    $validator = Validator::make($request->all(), [
+          'adi'=>'required|adi|unique:admins',
+          'sifre'=>'required|sifre|min:4|max:12',
+
+        ]);
+
+        $userInfo = admin::where('adi','=', $request->adi)->first();
+
+        if(!$userInfo){
+            return back()->with('fail','Numaraya kayıtlı admin yok');
+        }else{
+            //check password
+            if(admin::where($request->sifre, $userInfo->sifre)){
+                $request->session()->put('LoggedUser', $userInfo->id);
+                return redirect('k');
+
+            }else{
+                return back()->with('fail','sifre Yanlıs');
+            }
+        }
+    }
+    public function alma (Request $req)
+    {
+      $kullani = new musteri();
+      $kullani->ad=$req->ad;
+      $kullani->soyad=$req->soyad;
+      $kullani->email=$req->email;
+      $kullani->filmid=$req->filmid;
+      $kullani->biletid=$req->biletid;     
+      $kullani->salon=$req->salon;
+      $kullani->koltuk=$req->koltuk;
+      $kullani->saat=$req->saat;
+      $kullani->save(); 
+      
+      return view('koltuk');
+
+    }
+    public function koltuk (Request $req)
+    {
+      
+      
+     
+      return back();
+
+
+    }
+    public function goster()
+    {
+       
+       $film=DB::table('films')->get();            
+       return view('koltuk',['film'=> $film]);
+    }
+}
--- resources/views/dialog.blade.php
+++ resources/views/dialog.blade.php
@@ -10,19 +10,112 @@
 <table>
     
 
-         <form action="" method="post" action="{{route('musteri')}}">
-             @csrf
-         <tr><input type="text" placeholder="filmadı" name="filmid"></tr>
-    <tr> <td> salon no: </td><th> <input type="text" name="salon" value=""> </th></tr>
-    <tr> <td >koltuk no: </td><th> <input type="text" name="koltuk" value=""> </th></tr>
-    <tr> <td >saat: </td><th> <input type="text" name="saat" value=""> </th></tr>
-    <tr> <td >ücret: </td><th> <input type="text" name="ucret" value=""> </th></tr>
-    <tr><input type="text" placeholder="e-mail" name="email"></tr>
-        <tr><input type="text" placeholder="ad" name="ad"></tr>
-        <tr><input type="text" placeholder="soyad" name="soyad"></tr>
+<center>
+        
 
-  </table><br>
-  <button type="submit" name="btn_giris" class="btn btn-block btn-primary " onClick="location='/k'" > GİRİŞ YAP</button>
-</form>
+        <dialog id="koltuksec" style="width:60%; background-color:rgba(255, 255, 255, 0.932); border:1px dotted black; box-shadow: 0 0 6px 4px rgba(0, 0, 0, 0.404);border-radius: 8%;"  
+        >
+        <table class="resim">
+            <form method="">
+                @csrf
+            <tr><td><button class="submit" value="09:00" name="salon">1</button></td>
+        <td><button class="button">2</button></td>
+        <td><button class="button">3</button></td>
+        <td><button class="button">4</button></td>
+        <td><button class="button">5</button></td>
+        </tr></form>
+            
+        <tr>
+          <th></th><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th><th>8</th><th>9</th><th>10</th>
+        </tr>
+        
+        <tr >
+        <td><h1> A</h1></td>
+        <td><button class="button" id="a1" height="50px" width="50px" value="a1" name="koltuk">A1</td>
+        <td><button class="button" id="a2" height="50px" width="50px" >A2</td>
+        <td><button class="button" id="a3" height="50px" width="50px" >A3</td>
+        <td><button class="button" id="a4" height="50px" width="50px" >A4</td>
+        <td><button class="button" id="a5" height="50px" width="50px" >A5</td>
+        <td><button class="button" id="a6" height="50px" width="50px" >A6</td>
+        <td><button class="button" id="a7" height="50px" width="50px" >A7</td>
+        <td><button class="button" id="a8" height="50px" width="50px" >A8</td>
+        <td><button class="button" id="a9" height="50px" width="50px" >A9</td>
+        <td><button class="button" id="a10" height="50px" width="50px" >A10</td>
+        
+        </tr>
+        
+        <tr>
+        <td><h1> B</h1></td><td><button class="button" id="b1" height="50px" width="50px" select" value="a1" name="koltuk">B1</td>
+        <td><button class="button" id="b2" height="50px" width="50px" onclick="style="background: black"">B2</td>
+        <td><button class="button" id="b3" height="50px" width="50px" >B3</td>
+        <td><button class="button" id="b4" height="50px" width="50px" >B4</td>
+        <td><button class="button" id="a4" height="50px" width="50px" >B5</td>
+        <td><button class="button" id="a5" height="50px" width="50px" >B6</td>
+        <td><button class="button" id="a6" height="50px" width="50px" >B7</td>
+        <td><button class="button" id="a7" height="50px" width="50px" >B8</td>
+        <td><button class="button" id="a8" height="50px" width="50px" >B9</td>
+        <td><button class="button" id="a9" height="50px" width="50px" >B10</td>
+        <tr>
+        <td><h1> C</h1></td><td><button class="button" id="a1" height="50px" width="50px" select" value="a1" name="koltuk">C1</td>
+        <td><button class="button" id="a2" height="50px" width="50px" onclick="style="background: black"">C2</td>
+        <td><button class="button" id="a3" height="50px" width="50px" >C3</td>
+        <td><button class="button" id="a4" height="50px" width="50px" >C4</td>
+        <td><button class="button" id="a4" height="50px" width="50px" >C5</td>
+        <td><button class="button" id="a5" height="50px" width="50px" >C6</td>
+        <td><button class="button" id="a6" height="50px" width="50px" >C7</td>
+        <td><button class="button" id="a7" height="50px" width="50px" >C8</td>
+        <td><button class="button" id="a8" height="50px" width="50px" >C9</td>
+        <td><button class="button" id="a9" height="50px" width="50px" >C10</td>
+        </tr>
+        <tr>
+        
+        <td><h1> D</h1></td><td><button class="button" id="a1" height="50px" width="50px" select" value="a1" name="koltuk">D1</td>
+        <td><button class="button" id="a2" height="50px" width="50px" onclick="style="background: black"">D2</td>
+        <td><button class="button" id="a3" height="50px" width="50px" >D3</td>
+        <td><button class="button" id="a4" height="50px" width="50px" >D4</td>
+        <td><button class="button" id="a4" height="50px" width="50px" >D5</td>
+        <td><button class="button" id="a5" height="50px" width="50px" >D6</td>
+        <td><button class="button" id="a6" height="50px" width="50px" >D7</td>
+        <td><button class="button" id="a7" height="50px" width="50px" >D8</td>
+        <td><button class="button" id="a8" height="50px" width="50px" >D9</td>
+        <td><button class="button" id="a9" height="50px" width="50px" >D10</td>
+        </tr>
+        
+            <table>
+            
+        
+                 <form action="" method="post" >
+                     @csrf
+                 <tr><input type="hidden" placeholder="filmadı" name="filmid"></tr>
+            <tr> <td> salon no: </td><th> <input type="text" name="salon" value=""> </th></tr>
+            
+            <tr> <td >saat: </td><th> <input type="text" name="saat" value=""> </th></tr>
+            <tr> <td >ücret: </td><th> <input type="text" name="ucret" value=""> </th></tr>
+            <tr><input type="text" placeholder="e-mail" name="email"></tr>
+                <tr><input type="text" placeholder="ad" name="ad"></tr>
+                <tr><input type="text" placeholder="soyad" name="soyad"></tr>
+        
+          </table><br>
+          <button type="submit" name="btn_giris" class="btn btn-block btn-primary " onClick="location='/k'" > GİRİŞ YAP</button>
+        </form>
+        <table>
+            <form action="" method="post"></form>
+            <tr>
+         <td><button class="submit" value="09:00" name="saat">09:00</button></td>
+        <td><button class="button">10:00</button></td>
+        <td><button class="button">11:00</button></td>
+        <td><button class="button">12:00</button></td>
+        <td><button class="button">13:00</button></td>
+        <td><button class="button">14:00</button></td>
+        <td><button class="button">15:00</button></td>
+        <td><button class="button">16:00</button></td>
+        </tr></table>
+        </table>
+        
+        
+        <button id="hide">Pencereyi Kapat</button>
+        </dialog></form>
+        </center>
+        
 </body>
 </html>
